338b4db9f3473375d3631e17a8444a2dc22a4d35
Vishnuskrishna <50326855+Vishnuskrishna@users.noreply.github.com>
2019-06-14 12:18:39 +0530

    number of words

--- Documents/python itern/reffernce.py
+++ Documents/python itern/reffernce.py
@@ -1,169 +1,15 @@
-try :   
-    import pyaudio
-    import wave
-    import numpy as np
-    import scipy.io.wavfile
-    import math
-    import numpy as np
-    import pandas as pd
-    import scipy.io.wavfile
-    import math
-    from pandas import ExcelWriter
-    import matplotlib.pyplot as plt
-    from pandas import ExcelFile
-    import warnings
-    
-    import json
-    import os
-    import re
-    import sys
-    from subprocess import Popen, PIPE
-    from math import log, ceil
-    from tempfile import TemporaryFile
-    from warnings import warn
-    from functools import wraps
-    from pydub import AudioSegment 
-    import speech_recognition as sr 
-    from pydub.playback import play
-except:
-    print("something not installed")
-
-
-warnings.simplefilter('ignore', DeprecationWarning)
-i=0
-f,ax = plt.subplots(2)
-
-# Prepare the Plotting Environment with random starting values
-x = np.arange(10000)
-y = np.random.randn(10000)
-
-# Plot 0 is for raw audio data
-li, = ax[0].plot(x, y)
-ax[0].set_xlim(0,1000)
-ax[0].set_ylim(-5000,5000)
-ax[0].set_title("Raw Audio Signal")
-# Plot 1 is for the FFT of the audio
-li2, = ax[1].plot(x, y)
-ax[1].set_xlim(0,5000)
-ax[1].set_ylim(-100,100)
-ax[1].set_title("Fast Fourier Transform")
-# Show the plot, but without blocking updates
-plt.pause(0.01)
-plt.tight_layout()
-outputFile = "outputFigure.png"
-FORMAT = pyaudio.paInt16 # We use 16bit format per sample
-CHANNELS = 2
-RATE = 44100
-CHUNK = 1024 # 1024bytes of data red from a buffer
-RECORD_SECONDS = 5
-WAVE_OUTPUT_FILENAME = "output.wav"
-
-p = pyaudio.PyAudio()
-frames=[]
-global keep_going
-keep_going = True
-stream = p.open(format=FORMAT,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                frames_per_buffer=CHUNK)
-
-print("* recording")
-Value=int(RATE / CHUNK * RECORD_SECONDS)
-frames = []
-def plot_data(in_data):
-    # get and convert the data to float
-    audio_data = np.fromstring(in_data, np.int16)
-    
-    # Fast Fourier Transform, 10*log10(abs) is to scale it to dB
-    # and make sure it's not imaginary
-    dfft = 10.*np.log10(abs(np.fft.rfft(audio_data)))
-
-    # Force the new data into the plot, but without redrawing axes.
-    # If uses plt.draw(), axes are re-drawn every time
-    #print audio_data[0:10]
-    #print dfft[0:10]
-    #print
-    li.set_xdata(np.arange(len(audio_data)))
-    li.set_ydata(audio_data)
-    li2.set_xdata(np.arange(len(dfft))*10.)
-    li2.set_ydata(dfft)
-
-    # Show the updated plot, but without blocking
-    plt.pause(0.02)
-    
-try:
-    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-   
-        Streaming=stream.read(CHUNK)
-        plot_data(Streaming)
-        frames.append(Streaming)
-    plt.close(f)
-    
-    print("* done recording")
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-    
-    
-
-    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-    wf.setnchannels(CHANNELS)
-    wf.setsampwidth(p.get_sample_size(FORMAT))
-    wf.setframerate(RATE)
-    wf.writeframes(b''.join(frames))
-    wf.close()
-    
-
-    rate, data = scipy.io.wavfile.read('output.wav')
-    
-    data2 = []
-    
-
-
-    for i in range(len(data)):
-        
-        data2.append([int(round(math.sin(data[i][0])*3000)), int(round(math.sin(data[i][1])*3000))])
-       
-
-    data2 = np.asarray(data2)
-
-    
-    
-    #plotting
-    spf = wave.open(WAVE_OUTPUT_FILENAME,'r')
-    signal = spf.readframes(-1)
-    signal = np.fromstring(signal, 'Int16')
-
-    fig = plt.figure(1)
-    plt.title('Signal Wave...')
-    plt.plot(signal)
-    
-
-    fig.savefig(outputFile)
-
-
-    rate, data = scipy.io.wavfile.read('output.wav')
-
-    data2 = []
-    
-    for i in range(len(data)):
-        data2.append([int(round(math.sin(data[i][0])*3000)), int(round(math.sin(data[i][1])*3000))])
-
-    data2 = np.asarray(data2)
-
-
-    
-    df=pd.DataFrame(data2)
-    df.columns=["x","y"]
-    writer = ExcelWriter('wave.xlsx')
-    df.to_excel(writer,'Sheet1',index=False)
-    
-    writer.save()
-except:
-    print("something went ")
-
-    
+import json
+import os
+import re
+import sys
+from subprocess import Popen, PIPE
+from math import log, ceil
+from tempfile import TemporaryFile
+from warnings import warn
+from functools import wraps
+from pydub import AudioSegment 
+import speech_recognition as sr 
+from pydub.playback import play
   
 # Input audio file to be sliced 
 audio = AudioSegment.from_wav("output.wav") 
@@ -197,7 +43,7 @@ def detect_silence(audio_segment, min_silence_len=1000, silence_thresh=-22, seek
 
     # convert silence threshold to a float value (so we can compare it to rms)
     silence_thresh = db_to_float(silence_thresh) * audio_segment.max_possible_amplitude
-    
+    print(silence_thresh)
 
     # find silence and add start and end indicies to the to_cut list
     silence_starts = []
@@ -317,5 +163,4 @@ def detect_leading_silence(sound, silence_threshold=-50.0, chunk_size=10):
 Chunks=split_on_silence(audio_segment)
 for voice in Chunks:
     play(voice)
-
 
